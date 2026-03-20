320b76e8e8c9a945507870cbc5db7d8fa7c949dd
yatin sharma <yatins113@gmail.com>
2026-03-20 15:13:52 +0530

    update product view changes

--- app/admin/edit/[id]/page.tsx
+++ app/admin/edit/[id]/page.tsx
@@ -105,7 +105,7 @@ export default function EditProduct() {
     })
 
     const res = await ApiFetch(`/api/products/update/${id}/`,{
-       method: "POST",
+       method: "PUT",
   body: formData
     })
     
