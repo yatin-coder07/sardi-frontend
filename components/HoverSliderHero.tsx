571fb2e259cef08748223a42222a45fa5c60a9e1
yatin sharma <yatins113@gmail.com>
2026-03-26 11:11:29 +0530

    design changes

--- components/HoverSliderHero.tsx
+++ components/HoverSliderHero.tsx
@@ -30,24 +30,24 @@ const SLIDES = [
 
 export function HoverSliderHero () {
   return (
-    <HoverSlider className="w-full h-screen px-6 md:px-16 lg:px-24  text-[#3d3929] flex items-center ml-20">
+    <HoverSlider className="w-full min-h-screen px-4 md:px-16 lg:px-24 text-[#3d3929] flex items-center md:ml-20">
      
-      <div className="w-full flex items-center justify-between gap-10">
+      <div className="w-full flex flex-col md:flex-row items-center justify-between gap-6 md:gap-10 py-10 md:py-0">
 
         {/* LEFT TEXT */}
-        <div className="flex flex-col space-y-4 md:space-y-6 w-1/2">
+        <div className="flex flex-col space-y-2 md:space-y-4 md:space-y-6 w-full md:w-1/2">
           {SLIDES.map((slide, index) => (
             <TextStaggerHover
               key={slide.title}
               index={index}
-              className="cursor-pointer text-5xl md:text-6xl lg:text-6xl font-bold uppercase tracking-tight leading-none"
+              className="cursor-pointer text-3xl sm:text-2xl md:text-6xl lg:text-6xl font-bold uppercase tracking-tight leading-none"
               text={slide.title}
             />
           ))}
         </div>
 
         {/* RIGHT IMAGE */}
-        <div className="w-1/2 h-full flex items-center justify-center">
+        <div className="w-full md:w-1/2 h-full flex items-center justify-center">
           <HoverSliderImageWrap>
             {SLIDES.map((slide, index) => (
               <div key={slide.id} className="w-full h-full flex items-center justify-center">
@@ -56,7 +56,7 @@ export function HoverSliderHero () {
                   imageUrl={slide.imageUrl}
                   src={slide.imageUrl}
                   alt={slide.title}
-                  className="w-full h-[80vh] object-contain"
+                  className="w-full h-[40vh] md:h-[80vh] object-contain"
                   loading="eager"
                   decoding="async"
                 />
